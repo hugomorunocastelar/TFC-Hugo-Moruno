e1faceb239a39b814dc307caa767024d9554f711
Hugo Moruno <hugomoruno2003@gmail.com>
2025-03-26 20:15:06 +0100

    26 - Marzo | Inicio Fase 2 - Adición de Microservicios de ejemplo

---
+++ aplicaciones/dot&dot-server/src/main/java/dot/server/serverApp/ServerAppApplication.java
@@ -0,0 +1,13 @@
+package dot.server.serverApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerAppApplication.class, args);
+	}
+
+}
